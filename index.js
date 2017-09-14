2d3436cc84f8634b415944279916ad0c85c335f2
Jacob Bass <jacob@jacobbass.net>
2017-09-14 22:22:12 +1000

    prettier

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 module.exports = {
-  "extends": "eslint-config-airbnb"
+  "extends": [
+    "eslint-config-airbnb",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react",
+  ],
+  "plugins": [
+    "flowtype",
+    "react",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "rules": {
+    "prettier/prettier": ["error", {"singleQuote": true, "trailingComma": "es5"}]
+  }
 };
